e6bfd404b90a6c31f22de77e054888fd2d3da8cb
chunhatpham <jickjame84@gmail.com>
2026-03-12 01:58:47 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Một Service Worker trống hoặc cơ bản nhất để pass điều kiện của PWA
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-});
-
-self.addEventListener('fetch', (e) => {
-    // Không làm gì cả, chỉ để trình duyệt biết là có file này
-});
